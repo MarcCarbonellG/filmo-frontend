357ecaa7e45a40344926b2ae677274b5047d08a9
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2025-05-31 18:38:31 +0100

    Fixing errors

--- src/app/movie/pages/search-results/search-results.component.ts
+++ src/app/movie/pages/search-results/search-results.component.ts
@@ -64,7 +64,7 @@ export class SearchResultsComponent implements OnInit {
     this.isLangDropdownOpen = !this.isLangDropdownOpen;
   }
 
-  loadMovies(page: number) {
+  loadMovies(page: number = 1) {
     this.movieService.searchMoviesByTitle(this.searchTerm, page).subscribe({
       next: (data) => {
         this.originalResults = data;
